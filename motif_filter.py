0bf9e71ba12b71e0e5678a4bc192ff22f7ecd1b0
Marina <marina.sarrias@gmail.com>
2019-02-05 15:04:38 +0100

    update motif filter files handling

--- motif_filter.py
+++ motif_filter.py
@@ -1,55 +1,50 @@
 import sys
 import re
+import subprocess
 import urllib.request
 from Bio.Seq import Seq, transcribe, translate
+from Bio.Align.Applications import MuscleCommandline
 from Bio import SeqIO
-
-def checking_for_errors_in_filename(filenames_list):
-    """
-    checking_for_errors_in_filename is a function that evaluates two scenarios in which the file reading could go wrong.
-    :param filenames_list: is a list containing filenames passed as arguments from the command line.
-    :return: Boolean and an error message, TRUE if there's an error with the file and FALSE otherwise.
-    """
-
-    for filename in filenames_list:
-        try:
-            #if the files opens correctly pass.
-            with open(filename, 'r') as my_file:
-                pass
-        # if it doesn't pass the exception as e
-        except Exception as e:
-            # return TRUE and e
-            return True, e
-        # if the filename extension is not 'fa' raise an error message.
-        if filename[-3:] != '.fa':
-            # return TRUE, error message
-            return True, 'only fasta format accepted'
-
-    return False, ''
 
 if __name__ == '__main__':
 
-    # filenames_list = list of entered arguments from command line.
     file_in = sys.argv[1]
     f_out = 'motif_sequence.fa'
-    # has_errors, error_message = checking_for_errors_in_filename(file_in)
-    # # if has_errors is TRUE
-    # if has_errors:
-    #     # print the error message and write it on stderr.
-    #     print(error_message, file=sys.stderr)
-    #     # raise Exception and stop the programs execution, message: 'wrong entry'.
-    #     raise Exception('wrong entry')
-
-    # Prosite notation: K-L-[EI]{2-}-K
     pattern = re.compile("KL[EI]{2,}K")
-    my_sequence_recorded = list(SeqIO.parse(open(file_in, mode='r'), 'fasta'))
-    # print(len(my_sequence_recorded_iterable))
+    muscle_exe = "muscle"
+    in_file = f_out
+    out_file = "out_motif_sequence.fa"
 
-    my_sequence_recorded_motif = []
-    for seq in my_sequence_recorded:
-        for match in re.finditer(pattern, str(seq.seq)):
-            my_sequence_recorded_motif += [seq]
-            print('>' + seq.id)
-            print(seq.seq)
+
+    if file_in[-3:] != '.fa':
+        sys.exit('WARNING: only fasta format accepted')
+
+    else:
+        my_sequence_recorded = list(SeqIO.parse(open(file_in, mode='r'), 'fasta'))
+
+    if (len(my_sequence_recorded)) == 0:
+        sys.exit('WARNING: Empty file')
+
+    else:
+
+        my_sequence_recorded_motif = []
+        for seq in my_sequence_recorded:
+            for match in re.finditer(pattern, str(seq.seq)):
+                my_sequence_recorded_motif += [seq]
+                print('>' + seq.id)
+                print(seq.seq)
+
+    print('     ')
+    print('The total number of sequences in this file is:')
+    print(len(my_sequence_recorded))
+    print('The total number of sequences containing the motif is:')
     print(len(my_sequence_recorded_motif))
+
+    muscle_result = subprocess.check_output([muscle_exe, "-in", in_file, "-out", out_file])
+    cline = MuscleCommandline(muscle_exe, input=in_file, out=out_file)
+
+    print('The multialignment file is now available in your directory.')
+
+
+
     SeqIO.write(my_sequence_recorded_motif, f_out, 'fasta')
